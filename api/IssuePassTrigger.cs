aa22f08be09cc612b00333d6a7ace25ebf9f94ed
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2025-11-12 17:15:22 +0000

    Adding first trigger

---
+++ api/IssuePassTrigger.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Sebug.Function
+{
+    public static class IssuePassTrigger
+    {
+        [FunctionName("IssuePassTrigger")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            string responseMessage = "Got the following information: " + requestBody;
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
